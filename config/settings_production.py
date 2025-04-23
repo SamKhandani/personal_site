35e92e2b305a0780ecf1bb90dd4244cb7938ace1
samkhandani <Sam.khandani.1387@gmail.com>
2025-04-24 01:35:08 +0330

    setting!

---
+++ config/settings_production.py
@@ -0,0 +1,33 @@
+from .settings import *
+import os
+import dj_database_url
+
+# Security settings
+DEBUG = False
+ALLOWED_HOSTS = ['*']  # Replace with your actual domain
+
+# Database
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
+
+# Static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Media files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+# Security
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True 
